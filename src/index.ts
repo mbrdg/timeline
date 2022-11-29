b83121e5caab66a4e790ecaf3da8a75f3c7125d8
Miguel Rodrigues <up201906042@edu.fe.up.pt>
2022-11-29 23:14:56 +0000

    Create http server in order to test the configuration of kademlia nodes ids.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 // SDLE @ M.EIC, 2022
-console.log('hello world');
+import { createServer } from "http";
+import Node from "./kademlia/node";
+
+const port = 8000;
+const self = new Node('localhost', port);
+
+createServer((req, res) => {
+
+    const { method, url } = req;
+
+    if (method === 'GET' && url === '/id') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.write(self.id)
+        res.end()
+    }
+
+    res.writeHead(404);
+    res.end();
+
+}).listen(port)
+
