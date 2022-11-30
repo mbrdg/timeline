1330c575e013bc3ef50bd5391ebb7ddff38f7c2e
Miguel Rodrigues <up201906042@edu.fe.up.pt>
2022-11-30 00:40:34 +0000

    Make params from node constructor readonly

--- src/kademlia/tlnode.ts
+++ src/kademlia/tlnode.ts
@@ -1,13 +1,13 @@
-// AC @ M.EIC, 2022
-// SDLE, T4G14
+// SDLE @ M.EIC, 2022
+// T4G14
 import { createHash } from "crypto";
 
 
-export default class Node {
+export default class TLNode {
 
     readonly id: string;
 
-    constructor(ip: string, port: number) {
+    constructor(readonly ip: string, readonly port: number) {
         this.id = createHash('sha1')
             .update(ip)
             .update(port.toString())
