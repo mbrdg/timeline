4bdfc4e480a099544d6fb3f3bbd267af25c7a946
Miguel Rodrigues <up201906042@edu.fe.up.pt>
2022-12-01 22:59:44 +0000

    Add MulticastDNS to allow peer discovery in the local network

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,37 @@
 // T4G14
 
 import express from 'express';
+import { AddressInfo } from 'net';
 import { createLibp2p } from 'libp2p';
 import { tcp } from '@libp2p/tcp';
 import { noise } from '@chainsafe/libp2p-noise';
+import { mdns } from '@libp2p/mdns';
 import { kadDHT } from '@libp2p/kad-dht';
-
+import type { PeerInfo } from '@libp2p/interface-peer-info';
 import TLPost from './tlpost.js';
 
 
 const main = async () => {
+
     const node = await createLibp2p({
         addresses: {
-            listen: ['/ip4/127.0.0.1/tcp/0']
+            listen: ['/ip4/0.0.0.0/tcp/0']
         },
         transports: [tcp()],
         connectionEncryption: [noise()],
+        peerDiscovery: [mdns()],
         dht: kadDHT()
+    });
+
+    node.addEventListener('peer:discovery', (event) => {
+        const peer = event.detail as PeerInfo;
+        console.log(`⚗️  Discovered peer ${peer.id}`);
     });
 
     await node.start();
     console.info(`🐦 libp2p node has started`);
 
-    const port = 8000;
+    const [hostname, port]  = ['localhost', 0];
     const app = express();
     app.use(express.json());
 
@@ -43,8 +52,9 @@ const main = async () => {
         res.status(201).send(`Tweet published successfully`);
     });
 
-    app.listen(port, () => {
-        console.info(`🐦 Server running at http://localhost:${port}`);
+    const httpServer = app.listen(port, hostname, () => {
+        const address = httpServer.address() as AddressInfo;
+        console.info(`🐦 Server running at http://${hostname}/${address.port}`);
     });
 }
 
