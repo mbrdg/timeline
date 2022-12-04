3ade2f1006194d551d6f6687dd938e9218a2cf3d
Tiago Silva <silvatigas@hotmail.com>
2022-12-04 21:00:03 +0000

    Create an interface to represent an interaction between a user and a post

--- src/tlpost.ts
+++ src/tlpost.ts
@@ -3,10 +3,17 @@
 
 import { TLUserHandle } from "./tluser.js";
 
+export type TLPostId = string;
+
 export default interface TLPost {
     readonly handle: TLUserHandle;
     readonly content: string;
     timestamp: Date;
     reposts: TLUserHandle[];
     likes: TLUserHandle[];
+}
+
+export interface TLInteraction {
+    readonly handle: TLUserHandle,
+    readonly postId: TLPostId
 }
