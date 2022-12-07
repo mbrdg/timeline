9de5e274a897582b75972931ab495a85f07c1bd5
201907877 <up201907877@fe.up.pt>
2022-12-07 19:44:02 +0000

    Add like and repost requests

--- src/client/src/components/Timeline.vue
+++ src/client/src/components/Timeline.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import type { Post } from '@/types/Post';
 import PostCard from '@/components/PostCard.vue'
-const props = defineProps({
-  posts: Array<Post>,
-  name: String
-});
+
+export interface Timeline {
+  posts: Map<string, Post>;
+  name: string;
+}
+const props = defineProps<Timeline>();
 </script>
 
 <template>
   <section class="self-start pt-10 w-1/2">
     <h1 class="font-bold text-5xl">Timeline</h1>
-    <div v-for="post in props.posts" class="flex flex-col">
-      <PostCard :post="post" :name="props.name" />
+    <div v-for="id in props.posts.keys()" class="flex flex-col">
+      <PostCard :post="props.posts.get(id)" :name="props.name" :id="id" />
     </div>
   </section>
 </template>
