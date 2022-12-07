60512234ad12c78fc920d40a9463520a8fc1a0d6
Tiago Silva <silvatigas@hotmail.com>
2022-12-07 10:09:29 +0000

    Fix bug of the cors call

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const main = async () => {
     const app = express();
 
     // CORS for all origins, what a beautiful flaw
-    app.use(cors);
+    app.use(cors());
     app.use(express.json());
 
     const createCID = (data: Readonly<Partial<TLUser | TLPost | TLTopic>>) => {
