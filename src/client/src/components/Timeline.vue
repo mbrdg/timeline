6989d83b22d51c7af7f2a58389420b480c629346
201907877 <up201907877@fe.up.pt>
2022-12-07 16:23:20 +0000

    Separate post card to dedicated component

--- src/client/src/components/Timeline.vue
+++ src/client/src/components/Timeline.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import type { Post } from './ProfileView.vue';
-
+import type { Post } from '@/types/Post';
+import PostCard from '@/components/PostCard.vue'
 const props = defineProps({
   posts: Array<Post>,
   name: String
@@ -10,29 +10,8 @@ const props = defineProps({
 <template>
   <section class="self-start pt-10 w-1/2">
     <h1 class="font-bold text-5xl">Timeline</h1>
-    <div v-for="post in posts" class="flex flex-col">
-      <div class="mx-2 mt-2" v-if="name !== post.handle" >
-        <div>{{ name }} reposted</div>
-      </div>
-      <div class="container flex flex-col bg-lightdark rounded-md p-5 my-2 gap-2 shadow-md">
-        <div class="flex flex-col justify-between">
-          <div class="font-semibold text-lg">{{ post.handle }}</div>
-          <div class="font-light text-sm">{{ post.timestamp.toLocaleString() }}</div>
-        </div>
-        <div>{{ post.content }}</div>
-        <div class="flex-grow h-px bg-dark mt-2 opacity-70"></div>
-        <div class="flex flex-row justify-around">
-          <div class="flex flex-row gap-1">
-            <div>🔁</div>
-            <div>{{ post.reposts.length }}</div>
-          </div>
-          <div class="flex flex-row gap-1">
-            <div>❤️</div>
-            <div>{{ post.likes.length }}</div>
-          </div>
-        </div>
-      </div>
-
+    <div v-for="post in props.posts" class="flex flex-col">
+      <PostCard :post="post" :name="props.name" />
     </div>
   </section>
 </template>
