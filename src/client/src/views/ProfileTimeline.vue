a07fd4a2d5e3e5ca8277fbd6cca6caa5efe82116
201907877 <up201907877@fe.up.pt>
2022-12-07 03:20:01 +0000

    Add profile timeline view with mocks

--- src/client/src/views/ProfileTimeline.vue
+++ src/client/src/views/ProfileTimeline.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
-import { ref } from "vue";
-export interface Timeline {}
-const props = defineProps();
+import type { Post } from './ProfileView.vue';
+
+export interface Timeline { }
+const props = defineProps({
+  posts: Array<Post>
+});
 </script>
 
 <template>
-  <section class="self-start pt-10">
+  <section class="self-start pt-10 w-1/2">
     <h1 class="font-bold text-5xl">Timeline</h1>
     <!-- TODO: Add the entries here -->
+    <div v-for="post in posts">
+
+      <div class="container flex flex-col bg-lightdark rounded-md p-5 m-2 gap-2 shadow-md">
+        <div class="flex flex-col justify-between">
+          <div class="font-semibold text-lg">{{ post.handle }}</div>
+          <div class="font-light text-sm">{{ post.timestamp.toLocaleString() }}</div>
+        </div>
+        <div>{{ post.content }}</div>
+        <div class="flex-grow h-px bg-dark mt-2 opacity-70"></div>
+        <div class="flex flex-row justify-around">
+          <div class="flex flex-row gap-1">
+            <div>🔁</div>
+            <div>{{ post.reposts.length }}</div>
+          </div>
+          <div class="flex flex-row gap-1">
+            <div>❤️</div>
+            <div>{{ post.likes.length }}</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </section>
 </template>
--- src/client/src/views/ProfileView.vue
+++ src/client/src/views/ProfileView.vue
@@ -4,6 +4,13 @@ import { useRoute } from "vue-router";
 import { AxiosInstance } from "axios";
 import ProfileDescription from "../components/ProfileDescription.vue";
 import ProfileTimeline from "./ProfileTimeline.vue";
+export interface Post {
+  handle: string;
+  content: string;
+  timestamp: Date;
+  reposts: string[];
+  likes: string[];
+}
 
 enum PostInteraction {
   POST,
@@ -22,18 +29,46 @@ interface UserInfo {
   following: string[];
   interactions: Interaction[];
 }
+const mockPost: Post = {
+  handle: "anti-jules",
+  content: "Vue é grande fixe",
+  timestamp: new Date("2022-12-07T03:24:00"),
+  reposts: ["Krypt0"],
+  likes: ["Krypt0"],
+}
+
+const mockPost1: Post = {
+  handle: "Krypt0",
+  content: "Aceita, jovem!",
+  timestamp: new Date("2022-12-07T03:24:00"),
+  reposts: [],
+  likes: ["anti-jules"],
+}
+
 
 const mockUser: UserInfo = {
   handle: "Krypt0",
   followers: ["Daniela", "Miro", "Julia", "Semis"],
   following: ["Daniela", "Miro", "Julia"],
-  interactions: [],
+  interactions: [
+    { handle: "anti-jules", id: "1", interaction: PostInteraction.POST, timestamp: new Date("2022-12-7T02:47") },
+    { handle: "Krypt0", id: "2", interaction: PostInteraction.POST, timestamp: new Date("2022-12-7T03:47") },
+    { handle: "Krypt0", id: "1", interaction: PostInteraction.REPOST, timestamp: new Date("2022-12-7T04:47") },
+    { handle: "Krypt0", id: "1", interaction: PostInteraction.LIKE, timestamp: new Date("2022-12-7T04:47") },
+
+  ],
 };
+
+const mockPostsMap : Map<string, Post> = new Map([
+  ['1', mockPost],
+  ['2', mockPost1]
+]);
 
 const api = inject("api") as AxiosInstance;
 const route = useRoute();
 const handle = ref("");
 const user = ref<UserInfo>();
+const posts = ref<(Post | undefined)[]>();
 handle.value = route.params.handle as string;
 
 async function fetchUserInfo(handle: string) {
@@ -41,16 +76,31 @@ async function fetchUserInfo(handle: string) {
   console.log("The users data is", data);
   user.value = data;
 }
-onBeforeMount(async () => await fetchUserInfo(handle.value));
+
+async function fetchUserPosts(handle: string) {
+  const interactions = mockUser.interactions;
+  const idPostsToShow = interactions
+    .filter((interaction) => interaction.interaction !== PostInteraction.LIKE)
+    .map((interaction) => interaction.id)
+    .filter((elem, index, self)=> {
+      return index === self.indexOf(elem);
+    });
+  
+  posts.value = idPostsToShow
+  .map((id: string)=>mockPostsMap.get(id));
+ 
+ console.log(posts.value);
+}
+onBeforeMount(async () => {
+  await fetchUserInfo(handle.value)
+  await fetchUserPosts(handle.value)
+}
+);
 </script>
 
 <template>
-  <main class="w-1/3 flex flex-col mx-auto">
-    <ProfileDescription
-      :name="handle"
-      :followers="user.followers"
-      :following="user.following"
-    />
-    <ProfileTimeline />
+  <main class="w-3/5 flex flex-col mx-auto">
+    <ProfileDescription :name="handle" :followers="user.followers" :following="user.following" />
+    <ProfileTimeline :posts=posts?.values() />
   </main>
 </template>
