6350a82f2db7077f1b08dcb8957cd4d75478e381
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-07 17:51:02 +0000

    add logo and name to app

--- src/client/src/views/HomeView.vue
+++ src/client/src/views/HomeView.vue
@@ -5,13 +5,18 @@ import { inject, onBeforeMount } from "vue";
 
 const api = inject("api") as AxiosInstance;
 function getTimeline(): void {
-  console.log("Working on this");
+  console.log("Get Timeline not yet implemented");
 }
 onBeforeMount(getTimeline);
 </script>
 
 <template>
-  <main class="flex flex-col gap-10">
+  <main class="w-1/3 m-auto h-full flex flex-col items-center pt-10 gap-10">
+    <section class="w-full flex gap-6 items-center">
+      <img src="../assets/alef.svg" />
+      <h1 class="text-5xl text-accent font-bold">Homepage</h1>
+    </section>
     <PostCreationCard />
+    <!-- Insert timeline here when available -->
   </main>
 </template>
